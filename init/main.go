ef21198d2bf585ea6ae08a1bc85acd1f49883d15
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-10-11 08:32:01 +0900

    feat: API Handler (#2)

--- init/main.go
+++ init/main.go
@@ -1,1 +1,19 @@
-package init
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("에러가 떠버렸는데요??")
+		panic(err)
+	}
+}
+
+func helloWorld(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Hello World")
+}
