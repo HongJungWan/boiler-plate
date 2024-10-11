96e6c82559fc4394f877a2502f00cece1f41bf76
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2024-10-11 09:04:58 +0900

    feat: toml

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,1 +1,20 @@
 package cmd
+
+import (
+	"boiler-plate/config"
+	"fmt"
+)
+
+type Cmd struct {
+	config *config.Config
+}
+
+func NewCmd(filePath string) *Cmd {
+	c := &Cmd{
+		config: config.NewConfig(filePath),
+	}
+
+	fmt.Println(c.config.Server.Port)
+
+	return c
+}
