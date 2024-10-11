289e0018dd5a7ffcfd908ca505954817ad7b94da
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-10-11 09:16:08 +0900

    feat: gin (#4)

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -2,17 +2,22 @@ package cmd
 
 import (
 	"boiler-plate/config"
+	"boiler-plate/network"
 	"fmt"
 )
 
 type Cmd struct {
-	config *config.Config
+	config  *config.Config
+	network *network.Network
 }
 
 func NewCmd(filePath string) *Cmd {
 	c := &Cmd{
-		config: config.NewConfig(filePath),
+		config:  config.NewConfig(filePath),
+		network: network.NewNetwork(),
 	}
+
+	c.network.ServerStart(c.config.Server.Port)
 
 	fmt.Println(c.config.Server.Port)
 
