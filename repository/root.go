0eb965a13aef2ff698c80219fb9f4ec9aa1fbf2e
HONG-JUNG-WAN <hong43ok@gmail.com>
2024-10-12 11:11:25 +0900

    feat: service, repository

--- repository/root.go
+++ repository/root.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"github.com/cloudwego/iasm/expr"
+	"sync"
+)
+
+var (
+	repositoryInit     sync.Once
+	repositoryInstance *expr.Repository
+)
+
+type Repository struct {
+}
+
+func NewRepository() *Repository {
+	repositoryInit.Do(func() {
+		repositoryInstance = &Repository{}
+	})
+
+	return repositoryInstance
+}
