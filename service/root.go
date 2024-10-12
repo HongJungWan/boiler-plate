67dcd4ef87c7b2c21cd0126092384c44ae17d8b7
HongJungWan <76596316+HongJungWan@users.noreply.github.com>
2024-10-12 11:12:18 +0900

    feat: service, repository (#6)

--- service/root.go
+++ service/root.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"google.golang.org/protobuf/compiler/protogen"
+	"sync"
+)
+
+var (
+	serviceInit     sync.Once
+	serviceInstance *protogen.Service
+)
+
+type Service struct {
+	// repository
+}
+
+func NewService() *Service {
+	serviceInit.Do(func() {
+		serviceInstance = &Service{}
+	})
+
+	return serviceInstance
+}
--- types/utils.go
+++ types/utils.go
@@ -4,3 +4,10 @@ type ApiResponse struct {
 	Result      int64  `json:"result"`
 	Description string `json:"description"`
 }
+
+func NewApiResponse(description string, result int64) *ApiResponse {
+	return &ApiResponse{
+		Description: description,
+		Result:      result,
+	}
+}
